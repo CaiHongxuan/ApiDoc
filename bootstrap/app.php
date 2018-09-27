e844ea4be358344a5ef4735b6d5547030b8546db
蔡俊贤 <1203673288@qq.com>
2018-09-27 23:47:34 +0800

    开放权限服务容器

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -64,9 +64,9 @@ $app->middleware([
 //    App\Http\Middleware\ExampleMiddleware::class
 ]);
 
-// $app->routeMiddleware([
-//     'auth' => App\Http\Middleware\Authenticate::class,
-// ]);
+$app->routeMiddleware([
+    'auth' => App\Http\Middleware\Authenticate::class,
+]);
 
 /*
 |--------------------------------------------------------------------------
@@ -83,7 +83,7 @@ $app->configure('cors');
 $app->register(Barryvdh\Cors\ServiceProvider::class);
 
 // $app->register(App\Providers\AppServiceProvider::class);
-// $app->register(App\Providers\AuthServiceProvider::class);
+$app->register(App\Providers\AuthServiceProvider::class);
 // $app->register(App\Providers\EventServiceProvider::class);
 
 /*
